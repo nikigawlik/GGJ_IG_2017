3df64c04ae91671cb6077bf1e4e28560e3fbff34
Nils <nilsgawlik@gmx.de>
2017-01-20 23:30:37 +0100

    player gravity

--- Assets/WaveSurf.cs
+++ Assets/WaveSurf.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class WaveSurf : MonoBehaviour {
 
+	public Queue<double> wave; // waves
+
+	private Vector2 velocity = new Vector2 (0, 0);
+
+	public float gravFactor;
+
+
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void FixedUpdate () {
+		Vector2 pos = this.transform.position;
+
+		velocity.y += getGravity();
+
+		this.transform.position = pos + velocity;
+		//this.transform.position += 100;
+	}
+
+	float getGravity() {
+		Vector2 pos = this.transform.position;
+
+		return pos.y * gravFactor * -1f;
 	}
 }
