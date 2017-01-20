34a96fe5481673f33ea734c887f910ca31130f9c
Laurihin <laurin.grossmann@outlook.de>
2017-01-21 00:12:24 +0100

    Wobbel weg; Benutzerfreundlichkeit boost

--- Assets/waveMotion.cs
+++ Assets/waveMotion.cs
@@ -4,30 +4,30 @@ using UnityEngine;
 
 public class waveMotion : MonoBehaviour
 {
-    //public Color c1 = Color.red;
-    //public int lineLength = 20;
-    public int rund;
     public int tiles;
+    public float amplitude = 1;
+    public float speed;
+    public float frequenz = 1F;
+
+    private float speedhub = 0;
 
 
     void Start()
     {
         LineRenderer lineRenderer = gameObject.GetComponent<LineRenderer>();
-        lineRenderer.numCornerVertices = rund;
-        lineRenderer.numCornerVertices = rund;
         lineRenderer.numPositions = tiles;
     }
 
 
     void Update()
     {
+        speedhub = speedhub + speed;
         LineRenderer lineRenderer = gameObject.GetComponent<LineRenderer>();
-        lineRenderer.numCornerVertices = rund;
-        lineRenderer.numCornerVertices = rund;
         int i = 0;
+        float currentTime = Time.time + speedhub;
         while (i < lineRenderer.numPositions)
         {
-            Vector3 pos = new Vector3(-10 + i*2, Mathf.Sin(i + Time.time));
+            Vector3 pos = new Vector3(-10 + i*frequenz, (Mathf.Sin(i + currentTime))*amplitude);
             lineRenderer.SetPosition(i, pos);
             i++;
         }
