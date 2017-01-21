b86c0b31c772a590fada127762179cc36636d80a
elkontexo <el_kontexo@arcor.de>
2017-01-21 04:27:47 +0100

    Shooting in "John"

---
+++ Assets/Scripts/canShoot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class canShoot : MonoBehaviour {
+
+	public GameObject BulletPrefab;
+	public Camera cam;
+	//Vector3 bulletSpeed;
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			//Rotation doesnt work for some reason *~*
+			GameObject bullet = Instantiate (BulletPrefab, this.transform.position, this.transform.rotation);
+			//Direction:
+			bullet.GetComponent<BulletController> ().speed = this.transform.rotation * Vector3.right;
+			bullet.GetComponent<BulletController> ().cam = this.cam;
+
+		
+		}
+
+	}
+}
---
+++ Assets/Scripts/hasLives.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hasLives : MonoBehaviour {
+
+	public int lives;
+
+	void die(){
+		//respawn
+		this.transform.position.x = 0;
+	}
+
+	void getHit(){
+		lives--;
+		if (lives <= 0) {
+			die ();
+		}
+
+
+	}
+}
