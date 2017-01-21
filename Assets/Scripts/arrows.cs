51d12a02d78cfb90bf87f43e731d31e457048ee5
Benjamin Petsch <petschbenjamin@gmail.com>
2017-01-21 22:24:23 +0100

    arrows working now

--- Assets/Scripts/arrows.cs
+++ Assets/Scripts/arrows.cs
@@ -4,22 +4,39 @@ using UnityEngine;
 
 public class arrows : MonoBehaviour {
 
+	public int playerNr = 0;
 	private Vector3 screenPos;
 	private Camera cam;
-	private GameObject dreieck;
+	private GameObject character;
+	private SpriteRenderer renderer;
+	public float objectSize = 0.33f;
 
 	// Use this for initialization
 	void Start () {
-		dreieck = gameObject.transform.GetChild (2).gameObject;
-		dreieck.SetActive (false);
+		renderer = gameObject.GetComponent<SpriteRenderer> ();
+		character = GameObject.FindGameObjectsWithTag("Player")[playerNr];
+		renderer.color = character.GetComponent<SpriteRenderer> ().color;
+		renderer.enabled = false;
 		cam = Camera.main;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		screenPos = cam.WorldToViewportPoint(transform.position);
-		//dreieck;
-		dreieck.SetActive (screenPos.y > 1 || screenPos.y < 0);
-
+		screenPos = cam.WorldToViewportPoint(character.transform.position);
+		if(screenPos.y>1.0f){
+			Debug.Log ("yay");
+			gameObject.GetComponent<SpriteRenderer> ().flipY = true;
+			transform.position = new Vector3 (character.transform.position.x,cam.orthographicSize-objectSize, 2.0f);
+			renderer.enabled = true;
+		}
+		else if (screenPos.y < 0.0f) {
+			gameObject.GetComponent<SpriteRenderer> ().flipY = false;
+			transform.position = new Vector3 (character.transform.position.x, -(cam.orthographicSize - objectSize), 2.0f);
+			renderer.enabled = true;
+		} else {
+			//Debug.Log (screenPos.y);
+			renderer.enabled = false;
+		}
 	}
+
 }
