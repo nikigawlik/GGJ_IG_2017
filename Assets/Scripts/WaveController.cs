b35ffdd21b5fd9e819b971b12bd19ec7c612a41b
Benjamin Petsch <petschbenjamin@gmail.com>
2017-01-21 12:45:52 +0100

    completed wave controller

--- Assets/Scripts/WaveController.cs
+++ Assets/Scripts/WaveController.cs
@@ -4,13 +4,54 @@ using UnityEngine;
 
 public class WaveController : MonoBehaviour {
 
+	public float amplitudeMax;
+	public float amplitudeMin;
+	public float amplitudeProbability;
+	public float timeSpeedMax;
+	public float timeSpeedMin;
+	public float timeSpeedProbability;
+	public float xVersatzMax;
+	public float xVersatzMin;
+	public float xVersatzProbability;
+	public float yVersatzMax;
+	public float yVersatzMin;
+	public float yVersatzProbability;
+	private WavePointGenerator wavePointGen;
+
 	// Use this for initialization
 	void Start () {
-		
+		wavePointGen = GetComponent<WavePointGenerator> ();
+	}
+
+	void changeParam(ref float param, ref float max, ref float min){
+		param = min + (max-min) * Random.value;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if ( ( Random.value * 100) > (100 - amplitudeProbability) ) {
+			changeParam (ref wavePointGen.targetAmplitudeOne, ref amplitudeMax, ref amplitudeMin);
+		}
+		if ( ( Random.value * 100) > (100 - amplitudeProbability) ) {
+			changeParam (ref wavePointGen.targetAmplitudeTwo, ref amplitudeMax, ref amplitudeMin);
+		}
+		if ( ( Random.value * 100) > (100 - timeSpeedProbability) ) {
+			changeParam (ref wavePointGen.targetTimeSpeedOne, ref timeSpeedMax, ref timeSpeedMin);
+		}
+		if ( ( Random.value * 100) > (100 - timeSpeedProbability) ) {
+			changeParam (ref wavePointGen.targetTimeSpeedTwo, ref timeSpeedMax, ref timeSpeedMin);
+		}
+		if ( ( Random.value * 100) > (100 - xVersatzProbability) ) {
+			changeParam (ref wavePointGen.targetXVersatzOne, ref xVersatzMax, ref xVersatzMin);
+		}
+		if ( ( Random.value * 100) > (100 - xVersatzProbability) ) {
+			changeParam (ref wavePointGen.targetXVersatzTwo, ref xVersatzMax, ref xVersatzMin);
+		}
+		if ( ( Random.value * 100) > (100 - yVersatzProbability) ) {
+			changeParam (ref wavePointGen.targetYVersatzOne, ref yVersatzMax, ref yVersatzMin);
+		}
+		if ( ( Random.value * 100) > (100 - yVersatzProbability) ) {
+			changeParam (ref wavePointGen.targetYVersatzTwo, ref yVersatzMax, ref yVersatzMin);
+		}
 	}
 }
