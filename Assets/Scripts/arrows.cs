1e1288c24136566ff7d2bf7aafe2921943f42d9f
Benjamin Petsch <petschbenjamin@gmail.com>
2017-01-21 20:40:06 +0100

    added 3pac and edited arrows a bit

--- Assets/Scripts/arrows.cs
+++ Assets/Scripts/arrows.cs
@@ -6,17 +6,23 @@ public class arrows : MonoBehaviour {
 
 	private Vector3 screenPos;
 	private Camera cam;
+	private GameObject dreieck;
 
 	// Use this for initialization
 	void Start () {
+		dreieck = gameObject.transform.GetChild (2).gameObject;
+		dreieck.SetActive (false);
 		cam = Camera.main;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		screenPos = cam.WorldToViewportPoint(transform.position);
-		if (screenPos.y > 1 || screenPos.y < 0) {
-			
-		}
+		dreieck.SetActive (screenPos.y > 1 || screenPos.y < 0);
+//		if (screenPos.y > 1 || screenPos.y < 0) {
+//			dreieck.SetActive (true);
+//		} else {
+//			dreieck.SetActive (false);
+//		}
 	}
 }
